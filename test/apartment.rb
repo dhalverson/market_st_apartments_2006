a37aabf75d2de0f6ed41687bec2ea9dd15a0c4ea
Daniel Halverson <daniel.halverson@hotmail.com>
2020-07-14 09:04:52 -0600

    Add apartment exists test

---
+++ test/apartment.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/renter'
+reqiore './lib/apartment'
+
+class ApartmentTest < Minitest::Test
+
+  def test_it_exists
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    assert_instance_of Apartment, unit1
+  end
+
+
+
+end
