ded093260e97715a7e6881fee6508fcf9f982522
Daniel Halverson <daniel.halverson@hotmail.com>
2020-07-14 09:11:34 -0600

    Add apartment renter method

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -3,12 +3,17 @@ class Apartment
   attr_reader :number,
               :monthly_rent,
               :bathrooms,
-              :bedrooms
+              :bedrooms,
+              :renter
   def initialize(args)
     @number = args[:number]
     @monthly_rent = args[:monthly_rent]
     @bathrooms = args[:bathrooms]
     @bedrooms = args[:bedrooms]
+    @renter = nil
   end
 
+  def add_renter(renter)
+    @renter = renter
+  end
 end
