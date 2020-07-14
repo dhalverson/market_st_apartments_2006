c5d214141e1a9cd38eeb39275ede8880321c28c0
Daniel Halverson <daniel.halverson@hotmail.com>
2020-07-14 09:10:43 -0600

    Add apartment add renter test

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -20,4 +20,13 @@ class ApartmentTest < Minitest::Test
     assert_equal 1, unit1.bedrooms
   end
 
+  def test_it_can_add_renter
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    renter1 = Renter.new("Jessie")
+
+    assert_nil(unit1.renter)
+    unit1.add_renter(renter1)
+    assert_equal renter1, unit1.renter
+  end
+
 end
