da10154907f6e90f149cf968851b77b0cfd2381e
Daniel Halverson <daniel.halverson@hotmail.com>
2020-07-14 09:56:18 -0600

    Add building highest rent test

--- test/building_test.rb
+++ test/building_test.rb
@@ -74,4 +74,21 @@ class BuildingTest < Minitest::Test
     assert_equal [unit2], building.rented_units
   end
 
+  def test_it_can_return_renter_with_highest_rent
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 1, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
+    renter1 = Renter.new("Spencer")
+    renter2 = Renter.new("Jessie")
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+    unit2.add_renter(renter1)
+    unit1.add_renter(renter2)
+
+    assert_equal renter2, building.renter_with_highest_rent
+  end
+
 end
